23d7e104b436293e3b928da6475346faea02c184
Bart Zwemmer | GIS Specialisten <bart@gisspecialisten.nl>
2021-12-24 14:02:54 +0100

    Fixed OSM download by city name

--- project/main.py
+++ project/main.py
@@ -1,10 +1,18 @@
 # Dask for parallel
-import os, sys
+import json
+import os
 import osm
 
-#SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-#sys.path.append(os.path.dirname(SCRIPT_DIR))
+# For running locally, use a dump in stead of querying the overpass API
+test_courts_json = 'C:/Users/BartZwemmerGISSpecia/Documents/bball-court-finder/training_data/courts_nl.json'
+if not os.path.isfile(test_courts_json):
+    print("Fetching data from OSM")
+    data = osm.downloadData()
+else:
+    print("Using local test data")
+    # Opening JSON file
+    d = open(test_courts_json, 'r')
+    data = json.loads(d.read())
 
-data = osm.downloadData()
-print(data)
+# Show JSON data
 osm.plotData(data)
--- project/osm.py
+++ project/osm.py
@@ -1,36 +1,51 @@
-#import overpy
 import json
+from geopy.geocoders import Nominatim
 import numpy as np
 import matplotlib.pyplot as plt
 import requests
 
-#api = overpy.Overpass()
+def getOSMId(cityname):
+  # Geocoding request via Nominatim
+  geolocator = Nominatim(user_agent="city_compare")
+  geo_results = geolocator.geocode(cityname, exactly_one=False, limit=3)
+  # Searching for relation in result set
+  for r in geo_results:
+    #print(r.address, r.raw.get("osm_type"))
+    if r.raw.get("osm_type") == "relation":
+        city = r
+        break
+
+  # Calculating area id from relation id
+  area_id = int(city.raw.get("osm_id")) + 3600000000
+
+  return area_id
 
 def downloadData():
+    # For testing, using Almere to verify fields
+    area_id = getOSMId('Almere')
+
     overpass_url = "http://overpass-api.de/api/interpreter"
+    """
+    geocodeArea is an address or locality. All data that intersects is retrieved
+    """
     overpass_query = """
-[timeout:600]
-[out:json]
-;
-area(3600047796)->.area_0;
-(
-  node
-    ["sport"="basketball"]
-    (area.area_0);
-  relation
-    ["sport"="basketball"]
-    (area.area_0);
-);
-(
-  ._;
-  >;
-);
-out;
-"""
+    [out:json] [timeout:600];
+    area(%s) -> .area_0;
+    (
+    way["sport"="basketball"](area.area_0);
+    relation["sport"="basketball"](area.area_0);
+    );
+    (._;>;);
+    out body;
+    """ % area_id
     response = requests.get(overpass_url, 
                         params={'data': overpass_query})
-    data = response.json()
-    return data
+    try:
+      data = response.json()
+      return data
+    except:
+      print("No data in reponse: "+str(response))
+      return None
 
 def plotData(data):
     # Collect coords into list
@@ -52,3 +67,16 @@ def plotData(data):
     plt.ylabel('Latitude')
     plt.axis('equal')
     plt.show()
+
+if __name__ == '__main__':
+  # Get OSM data
+  data = downloadData()
+  # Write data to file if it doesn't exist, so we don't need to re-download it with every run during development.
+  if data is not None:
+    with open('C:/Users/BartZwemmerGISSpecia/Documents/bball-court-finder/training_data/courts_nl.json', 'w') as outfile:
+        json.dump(data, outfile)
+    print("Data written")
+  else:
+    print("No data written")
+
+  
